7ca4b02cad2f401be82f1ab8466499bd1c9c1ea5
Mateusz Garbowski <fda0@matg.pl>
2026-01-10 04:19:54 +0100

    create outline propagate shader (unused)

--- src/shaders/shader_outline_draw.hlsl
+++ src/shaders/shader_outline_draw.hlsl
@@ -22,15 +22,12 @@ Fragment VertexShader(Vertex vert)
 
 Texture2D<float2> Texture : register(t0, space2);
 SamplerState Sampler : register(s0, space2);
-cbuffer FragmentUniform : register(b0, space3)
-{
-  float2 Resolution;
-};
 
 float4 FragmentShader(Fragment frag) : SV_Target0
 {
-  float2 uv = frag.vertex_p.xy / Resolution;
-  // float2 outline_uv = Texture.Load(int3(frag.vertex_p.xy, 0));
-  float2 outline_uv = Texture.Sample(Sampler, float3(uv, 0.0));
-  return float4(max(float2(0.0, 0.0), outline_uv), uv.x, 1.0);
+  float2 outline_uv = Texture.Load(int3(frag.vertex_p.xy, 0));
+  if (outline_uv.x < 0.0 || outline_uv.y < 0.0) {
+    discard;
+  }
+  return float4(max(float2(0.0, 0.0), outline_uv), 0.0, 1.0);
 }
---
+++ src/shaders/shader_outline_propagate.hlsl
@@ -0,0 +1,52 @@
+struct Vertex
+{
+  uint index : SV_VertexID;
+};
+
+struct Fragment
+{
+  float4 vertex_p : SV_Position;
+};
+
+Fragment VertexShader(Vertex vert)
+{
+  float4 positions[3] = {
+    float4(-1.0, -1.0, 0.0, 1.0),
+    float4( 3.0, -1.0, 0.0, 1.0),
+    float4(-1.0,  3.0, 0.0, 1.0),
+  };
+  Fragment frag;
+  frag.vertex_p = positions[vert.index % 3];
+  return frag;
+}
+
+Texture2D<float2> Texture : register(t0, space2);
+SamplerState Sampler : register(s0, space2);
+
+float2 FragmentShader(Fragment frag) : SV_Target0
+{
+  float2 frag_xy = frag.vertex_p.xy;
+  int2 frag_xy_int = int2(frag_xy);
+
+  float2 best_xy = float2(-1.0, -1.0);
+  float best_distsq = 1024.0 * 1024.0 * 1024.0;
+
+  for (int y = -1; y <= 1; y++)
+  {
+    for (int x = -1; x <= 1; x++)
+    {
+      float2 candidate_xy = Texture.Load(int3(frag_xy_int + int2(x, y), 0));
+      if (candidate_xy.x > 0.0)
+      {
+        float candidate_distsq = dot(frag_xy, candidate_xy);
+        if (candidate_distsq < best_distsq)
+        {
+          best_xy = candidate_xy;
+          best_distsq = candidate_distsq;
+        }
+      }
+    }
+  }
+
+  return best_xy;
+}
